c098078c10cf2792d692427e5908046f85dedd9f
zjacky0409 <0409jacky0409@gmail.com>
2022-10-18 23:03:25 +0800

    remove unused var

--- frontend/src/Layout/MainLayout.tsx
+++ frontend/src/Layout/MainLayout.tsx
@@ -38,7 +38,7 @@ const MainLayout = (props: PropsType) => {
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
