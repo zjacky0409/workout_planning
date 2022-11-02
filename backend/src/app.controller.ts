004af81f4adfd258037f669b77e223c8ad982b45
Jacky Luo (AGIC) <jacky.luo@agic.hk>
2022-11-02 10:40:54 +0800

    add role based auth

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { Roles } from './role_checker/roles.decorator';
+import { Role } from './role_checker/role.enum';
+import { RolesGuard } from './role_checker/role.guard';
 
 @Controller()
 export class AppController {
@@ -24,6 +27,14 @@ export class AppController {
   @UseGuards(JwtAuthGuard)
   @Post('auth/getUser')
   async getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('test/role')
+  onlyAdmin(@Request() req) {
+    console.log('someone is an admin')
     return req.user;
   }
 }
